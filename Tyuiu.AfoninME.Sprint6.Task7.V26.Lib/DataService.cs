419464b26c4bdf539461a243b6860823020f742f
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-12-25 20:44:25 +0500

    7

--- Tyuiu.AfoninME.Sprint6.Task7.V26.Lib/DataService.cs
+++ Tyuiu.AfoninME.Sprint6.Task7.V26.Lib/DataService.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Globalization;
-using System.IO;
-using System.Linq;
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.AfoninME.Sprint6.Task7.V26.Lib
 {
-    public class DataService
+    public class DataService: ISprint6Task7V26
     {       
         public int[,] GetMatrix(string path)
         {
